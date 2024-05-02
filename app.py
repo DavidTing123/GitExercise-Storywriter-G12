27253c3e0cc5cd49189b0c50edc1aa315704b7a3
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-02 12:27:07 +0800

    feat: added a route for sign up page

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+#https://youtu.be/pJ8V51XJuf0?si=JHO1ROHlo5-LgVa4 (An introduction to Python and Flask Templates) tutorial video that I learn from and reference from
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def login():
     return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 

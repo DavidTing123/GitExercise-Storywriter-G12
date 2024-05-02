a27ea8c898c4a7c517677101b6d77b3849dea191
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-02 18:56:48 +0800

    added config

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
 #https://youtu.be/pJ8V51XJuf0?si=JHO1ROHlo5-LgVa4 (An introduction to Python and Flask Templates) tutorial video that I learn from and reference from
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for, session
+from flask_mysqldb import MySQL
+from flask_login import UserMixin
+import MySQLdb.cursors
+import re
+
+
+
+
 app = Flask(__name__)
 
-@app.route("/")
+
+app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_USER"] ='root'
+app.config["MYSQL_PASSWORD"] ='Cyc2255!'
+app.config["MYSQL_DB"] ='sign up'
+
+mysql = MySQL(app)
+
+@app.route("/" , methods =["GET" , "POST"])
 def login():
     return render_template("login.html")
 

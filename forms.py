6c0cfe4da92d733562f44f53418ebc4005742a1c
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-08 23:56:34 +0800

    feat: allows input field to be cleared

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,SubmitField, BooleanField
+from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class SignUpForm(FlaskForm):
@@ -14,5 +14,4 @@ class SignUpForm(FlaskForm):
 class LogInForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=5, max=20)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
-    remember =BooleanField("Remember Me")
     submit = SubmitField("Log In")

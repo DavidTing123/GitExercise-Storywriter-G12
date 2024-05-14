619b01e195f06276d93b2b106c8b533514ceff54
DavidTing123 <167272484+DavidTing123@users.noreply.github.com>
2024-05-14 16:25:31 +0800

    added files

---
+++ run.py
@@ -0,0 +1,31 @@
+# Program name: StoryApp.py
+#
+# Description: A flask web application for story writer
+#
+# References:
+#  1. Flask – (Creating first simple application)
+#      https://www.geeksforgeeks.org/flask-creating-first-simple-application/
+#  2. Story Generator App Using Python
+#      https://www.geeksforgeeks.org/story-generator-app-using-python/
+#  3. How to build a Random Story Generator using Python?
+#      https://www.geeksforgeeks.org/how-to-build-a-random-story-generator-using-python/
+#  4. HTML Form Elements
+#     https://www.w3schools.com/html/html_form_elements.asp
+#  5. Python Text To Speech | pyttsx module
+#      https://www.geeksforgeeks.org/python-text-to-speech-pyttsx-module/?ref=lbp
+#  6. Build a Text to Speech Service with Python Flask Framework
+#      https://dev.to/siddheshshankar/build-a-text-to-speech-service-with-python-flask-framework-3966
+#  7. Flask - Calling python function on button OnClick event
+#      https://stackoverflow.com/questions/42601478/flask-calling-python-function-on-button-onclick-event
+#  8. 10 Lines Short Stories With Moral Lessons for Kids
+#      https://ofhsoupkitchen.org/short-stories-with-morals#:~:text=The%20Dog%20and%20the%20Bone,a%20bone%20in%20its%20mouth.
+#  9. CSS Links
+#      https://www.w3schools.com/css/css_link.asp
+# 10. CSS Text Shadow
+#      https://www.w3schools.com/css/css_text_shadow.asp
+#
+
+from StoryApp import app
+
+if __name__ == '__main__':
+    app.run(debug=True)

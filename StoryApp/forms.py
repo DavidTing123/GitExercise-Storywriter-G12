ee2f9536c8c3330a5d1ca10f9079b242acca09a5
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-16 22:37:06 +0800

    feat: added an update profile button

--- StoryApp/forms.py
+++ StoryApp/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
@@ -30,6 +31,7 @@ class LogInForm(FlaskForm):
 
 class UpdateProfileForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg','png'])])
     username = StringField("Username", validators=[DataRequired(), Length(min=5, max=20)])
     submit = SubmitField("Update")
 

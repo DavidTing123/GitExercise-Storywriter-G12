2bfeb19c3ab2ac2b0aa5ad94bca29d318ae3abf1
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-19 01:07:10 +0800

    changed import statement for serializer

--- StoryApp/models.py
+++ StoryApp/models.py
@@ -1,5 +1,5 @@
 from flask import current_app
-from itsdangerous import URLSafeTimedSerializer as Serializer
+from itsdangerous import URLSafeTimedSerializer 
 from StoryApp import db, login_manager
 from flask_login import UserMixin
 
@@ -15,12 +15,12 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
     
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'], )
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'], )
         try:
             user_id = s.loads(token)['user_id']
         except:

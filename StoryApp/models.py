3eca4a1403b603851dd3639737d48fe56c979b71
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-19 13:58:55 +0800

    added a column for bio in database

--- StoryApp/models.py
+++ StoryApp/models.py
@@ -13,6 +13,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20),unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
+    bio = db.Column(db.Text)
+    
 '''
     #def get_reset_token(self):
         s = URLSafeTimedSerializer(app.config['SECRET_KEY'])

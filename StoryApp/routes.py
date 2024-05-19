4cc986eba5204454b1de9d490ba8ded1cc839999
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-19 13:06:25 +0800

    feat: add css and change layout for profile page

--- StoryApp/routes.py
+++ StoryApp/routes.py
@@ -3,7 +3,7 @@ import smtplib
 import secrets 
 from PIL import Image
 from flask import render_template,url_for, request, flash, redirect
-from StoryApp import app,db, bcrypt, mail
+from StoryApp import app,db, bcrypt
 from StoryApp.forms import SignUpForm, LogInForm, UpdateProfileForm, RequestResetForm, ResetPasswordForm
 from StoryApp.models import User
 from flask_login import login_user, current_user, logout_user,login_required

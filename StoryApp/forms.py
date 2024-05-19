0da68cd9e543e5812461208cc02dc5b834aab8b7
Cheng Ying Chen <chengyc25@gmail.com>
2024-05-19 13:12:44 +0800

    feat: added a bio field at profile

--- StoryApp/forms.py
+++ StoryApp/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField,SubmitField
+from wtforms import StringField, PasswordField,SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from StoryApp.models import User
 
@@ -33,6 +33,7 @@ class UpdateProfileForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg','png'])])
     username = StringField("Username", validators=[DataRequired(), Length(min=5, max=20)])
+    bio = TextAreaField('Bio')
     submit = SubmitField("Update")
 
     def validate_email(self, email):
